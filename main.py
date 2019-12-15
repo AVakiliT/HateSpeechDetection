e3a9525f9bff32bbb8fd0eba167253965f6662c9
Amir Vakili <amirvt92@gmail.com>
2019-12-15 16:52:53 +0330

    Seperated FF and Transformer classes; Added train weights; Added F1 and P metrics

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+import random
+
 import torch
+from sklearn.metrics import confusion_matrix
+from torch import nn
 from torch.optim import AdamW
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
 from sklearn.model_selection import train_test_split
-from transformers import BertTokenizer, BertModel, DistilBertForSequenceClassification, AlbertTokenizer, AlbertForSequenceClassification
+from transformers import BertTokenizer, BertModel, DistilBertForSequenceClassification, AlbertTokenizer, \
+    AlbertForSequenceClassification, AlbertModel
 import tqdm
+from torch.nn import functional as F
 from tqdm import tqdm
 import pandas as pd
 import io
 import numpy as np
 import matplotlib.pyplot as plt
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# %%
+import argparse
+
+parser = argparse.ArgumentParser()
 
 # DATASET = 'Dataset/OffenseEval'
 # MAX_LEN = 60
@@ -18,23 +28,47 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # NUM_CLASSES = 4
 # BATCH_SIZE=32
 #
-# DATASET = 'Dataset/Emotion'
-# MAX_LEN = 60
-# N_EPOCHS = 5
-# NUM_CLASSES = 4
-# BATCH_SIZE=32
 
-DATASET = 'Dataset/Sarcasm'
-MAX_LEN = 60
-N_EPOCHS = 5
-NUM_CLASSES = 4
-BATCH_SIZE=32
+parser.add_argument('--dataset', default='Emotion')
+parser.add_argument('--max_len', default=60, type=int)
+parser.add_argument('--epochs', default=6, type=int)
+parser.add_argument('--num_classes', default=4, type=int)
+parser.add_argument('--batch_size', default=64, type=int)
+
+# parser.add_argument('--dataset', default='Sarcasm')
+# parser.add_argument('--max_len', default=60, type=int)
+# parser.add_argument('--epochs', default=6, type=int)
+# parser.add_argument('--num_classes', default=2, type=int)
+# parser.add_argument('--batch_size', default=64, type=int)
 
 
-#%%
+parser.add_argument('--device', default='cuda')
+parser.add_argument('--weight', action='store_false')
+parser.add_argument('--lr', default=2e-5, type=float)
+args = parser.parse_known_args()
+print(args)
+
+# %%
+MAX_LEN = args[0].max_len
+N_EPOCHS = args[0].epochs
+NUM_CLASSES = args[0].num_classes
+BATCH_SIZE = args[0].batch_size
+EPSILON = 10e-13
+LR = args[0].lr
+DATASET = 'Dataset/' + args[0].dataset
+WEIGHT = args[0].weight
+device = torch.device(args[0].device) if torch.cuda.is_available() else torch.device('cpu')
+
+SEED = 1
+torch.manual_seed(SEED)
+np.random.seed(SEED)
+random.seed(SEED)
+
+# %%
 
 df = pd.read_csv(DATASET + '/train.csv')
 df.dropna(inplace=True)
+label_counts = df.label.value_counts(normalize=True)
 
 df.text = df.text.apply(lambda x: "[CLS] " + x + ' [SEP]')
 
@@ -44,103 +78,148 @@ tokenized_texts = df.text.apply(lambda x: tokenizer.tokenize(' '.join(x.split()[
 
 token_ids = tokenized_texts.apply(tokenizer.convert_tokens_to_ids)
 
-token_ids_matrix = np.array(token_ids.apply(lambda x: x[:MAX_LEN] + [0] * max(0, MAX_LEN - len(x))).tolist()).astype('int64')
+token_ids_matrix = np.array(token_ids.apply(lambda x: x[:MAX_LEN] + [0] * max(0, MAX_LEN - len(x))).tolist()).astype(
+    'int64')
 
 attention_matix = (token_ids_matrix != 0).astype('float')
 
-train_dataset = TensorDataset(torch.tensor(token_ids_matrix), torch.tensor(attention_matix), torch.tensor(np.array(df.label)))
+train_dataset = TensorDataset(torch.tensor(token_ids_matrix), torch.tensor(attention_matix),
+                              torch.tensor(np.array(df.label)))
 
 train_data_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-
-#%%
+# %%
 df = pd.read_csv(DATASET + '/test.csv')
 
 df.text = df.text.apply(lambda x: "[CLS] " + x + ' [SEP]')
 
-tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2', do_lower_case=True)
-
 tokenized_texts = df.text.apply(lambda x: tokenizer.tokenize(' '.join(x.split()[:MAX_LEN])))
 
 token_ids = tokenized_texts.apply(tokenizer.convert_tokens_to_ids)
 
-token_ids_matrix = np.array(token_ids.apply(lambda x: x[:MAX_LEN] + [0] * max(0, MAX_LEN - len(x))).tolist()).astype('int64')
+token_ids_matrix = np.array(token_ids.apply(lambda x: x[:MAX_LEN] + [0] * max(0, MAX_LEN - len(x))).tolist()).astype(
+    'int64')
 
 attention_matix = (token_ids_matrix != 0).astype('float')
 
-test_dataset = TensorDataset(torch.tensor(token_ids_matrix), torch.tensor(attention_matix), torch.tensor(np.array(df.label)))
+test_dataset = TensorDataset(torch.tensor(token_ids_matrix), torch.tensor(attention_matix),
+                             torch.tensor(np.array(df.label)))
 
 test_data_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE)
 
-#%%
 
-model = AlbertForSequenceClassification.from_pretrained(
-    "albert-base-v2", num_labels=NUM_CLASSES)
+# %%
+class MyModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.transformer = AlbertModel.from_pretrained(
+            "albert-base-v2", num_labels=NUM_CLASSES)
+        self.ff = nn.Sequential(
+            nn.Linear(768, 100),
+            nn.ReLU(),
+            nn.Linear(100, NUM_CLASSES if NUM_CLASSES > 2 else 1)
+        )
+
+    def forward(self, batch_input, batch_mask):
+        xx = self.transformer(batch_input, batch_mask)
+        xx = self.ff(xx[0][:, 0, :]).squeeze()
+        return xx
+
+
+model = MyModel()
 model = model.cuda()
-
-#%%
+# %%
 param_optimizer = list(model.named_parameters())
 
-no_decay = ['bias', 'gamma', 'beta']
-optimizer_grouped_parameters = [
-    {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
-     'weight_decay_rate': 0.01},
-    {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
-     'weight_decay_rate': 0.0}
-]
+# no_decay = ['bias', 'gamma', 'beta']
+# optimizer_grouped_parameters = [
+#     {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+#      'weight_decay_rate': 0.01},
+#     {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+#      'weight_decay_rate': 0.0}
+# ]
 
 
-optimizer = AdamW(model.parameters(), lr=2e-5)
+optimizer = AdamW(model.parameters(), lr=LR)
 
-#%%
+
+# %%
+def calc_confusion_matrix(y, y_hat):
+    with torch.no_grad():
+        if NUM_CLASSES > 2:
+            pred = y_hat.argmax(-1)
+        else:
+            pred = y_hat.gt(0)
+
+    m = confusion_matrix(y.cpu(), pred.cpu(), range(NUM_CLASSES))
+    return m
+
+
+def calc_metrics(m):
+    p = (m.diagonal() / m.sum(0).clip(EPSILON)).mean()
+    r = (m.diagonal() / m.sum(1).clip(EPSILON)).mean()
+    f1 = ((2 * p * r) / (p + r)).mean()
+    accu = m.diagonal().sum() / m.sum()
+    return p, r, f1, accu
+
+
+train_weights = (1 / label_counts)
+train_weights = train_weights.to_numpy() / train_weights.sum()
+train_weights = torch.Tensor(train_weights).to(device)
+
+if WEIGHT:
+    if NUM_CLASSES > 2:
+        criterion = nn.CrossEntropyLoss(reduction='sum', weight=train_weights)
+    else:
+        criterion = nn.BCEWithLogitsLoss(reduction='sum', pos_weight=train_weights[1])
+else:
+    if NUM_CLASSES > 2:
+        criterion = nn.CrossEntropyLoss(reduction='sum')
+    else:
+        criterion = nn.BCEWithLogitsLoss(reduction='sum')
+
 train_loss_set = []
+metrics_history = []
+dev_metrics_history = []
 
-# trange is a tqdm wrapper around the normal python range
-for ep in range(N_EPOCHS):
-    print('EPOCH', ep)
+for i_epoch in range(N_EPOCHS):
 
-    # Training
-
-    # Set our model to training mode (as opposed to evaluation mode)
     model.train()
 
     # Tracking variables
     train_loss = 0
-    train_acc = 0
-    train_total, train_steps = 0, 0
+    train_confusion_table = np.zeros((NUM_CLASSES, NUM_CLASSES))
+    train_total = 0
 
     # Train the data for one epoch
-    p_bar =tqdm(train_data_loader)
+    p_bar = tqdm(train_data_loader)
     for step, batch in enumerate(p_bar):
-        # Add batch to GPU
         batch = tuple(t.to(device) for t in batch)
-        # Unpack the inputs from our dataloader
-        b_input_ids, b_input_mask, b_labels = batch
-        # Clear out the gradients (by default they accumulate)
+        batch_input, batch_mask, batch_y = batch
         optimizer.zero_grad()
-        # Forward pass
-        loss, logits = model(b_input_ids, attention_mask=b_input_mask, labels=b_labels)
 
-        train_acc += logits.argmax(1).eq(b_labels).long().sum().item()
-        train_loss_set.append(loss.item())
-        # Backward pass
+        logits = model(batch_input, batch_mask)
+
+        loss = criterion(logits, batch_y if NUM_CLASSES > 2 else batch_y.float())
         loss.backward()
-        # Update parameters and take a step using the computed gradient
+        # grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), GRAD_CLIP)
+        # assert grad_norm >= 0, 'encounter nan in gradients.'
         optimizer.step()
 
-        # Update tracking variables
+        train_confusion_table += calc_confusion_matrix(batch_y, logits)
+        train_total += batch_input.size(0)
         train_loss += loss.item()
-        train_total += b_input_ids.size(0)
-        train_steps += 1
 
+        metrics = (
+            train_loss / train_total,
+            *calc_metrics(train_confusion_table),
+        )
 
-        p_bar.set_description('Loss {:.4f} Acc {:.4f}'.format(train_loss / train_steps, train_acc / train_total))
+        p_bar.set_description(
 
-    print('\nTrain Loss {:.4f} Acc {:.4f}'.format(train_loss / train_steps, train_acc / train_total))
-
-
-
-    # Training
+            '[ EP {0:02d} ]'
+            '[ TRN LS: {1:.4f} PR: {2:.4f} F1: {4:.4f} AC: {5:.4f}]'
+                .format(i_epoch, *metrics))
 
     with torch.no_grad():
         # Set our model to testing mode (as opposed to evaluation mode)
@@ -148,29 +227,27 @@ for ep in range(N_EPOCHS):
 
         # Tracking variables
         test_loss = 0
-        test_acc = 0
-        test_total, test_steps = 0, 0
-
+        test_confusion_table = np.zeros((NUM_CLASSES, NUM_CLASSES))
+        test_total = 0
 
         # test the data for one epoch
         p_bar = tqdm(test_data_loader)
         for step, batch in enumerate(p_bar):
-            # Add batch to GPU
             batch = tuple(t.to(device) for t in batch)
-            # Unpack the inputs from our dataloader
-            b_input_ids, b_input_mask, b_labels = batch
-            # Clear out the gradients (by default they accumulate)
-            # Forward pass
-            loss, logits = model(b_input_ids, attention_mask=b_input_mask, labels=b_labels)
+            batch_input, batch_mask, batch_y = batch
 
-            test_acc += logits.argmax(1).eq(b_labels).long().sum().item()
+            logits = model(batch_input, batch_mask)
 
-
-            # Update tracking variables
+            test_confusion_table += calc_confusion_matrix(batch_y, logits)
+            test_total += batch_input.size(0)
             test_loss += loss.item()
-            test_total += b_input_ids.size(0)
-            test_steps += 1
 
-            p_bar.set_description('Loss {:.4f} Acc {:.4f}'.format(test_loss / test_steps, test_acc / test_total))
+            metrics = (
+                test_loss / test_total,
+                *calc_metrics(test_confusion_table),
+            )
 
-        print("\nTest Loss {:.4f} Acc {:.4f}".format(test_loss / test_steps, test_acc / test_total))
+            p_bar.set_description(
+                '[ EP {0:02d} ]'
+                '[ TST LS: {1:.4f} PR: {2:.4f} F1: {4:.4f} AC: {5:.4f}]'
+                    .format(i_epoch, *metrics))
